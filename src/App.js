48d1f91a902242df6649a6884c1bb449affc0220
Koichi.Shimomura <unieye51@gmail.com>
2022-07-28 16:24:18 +0900

    5-31 カスタムフック（分割）

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import React, { useState } from "react";
+import { CounterA } from "./counterA";
+import { CounterB } from "./counterB";
+import { CounterC } from "./counterC";
 import "./App.css";
 
-const useCounter = (initialCount) => {
-  const [count, setCount] = useState(initialCount);
-
-  const countAdd = () => setCount((prevCount) => prevCount + 1);
-  const countSub = () => setCount((prevCount) => prevCount - 1);
-  const countReset = () => setCount(initialCount);
-  return { count, countAdd, countSub, countReset };
-};
-
-const CounterText = ({ count }) => <p>現在のカウント数：{count}</p>;
-
-const Counter = () => {
-  const { count, countAdd, countSub, countReset } = useCounter(0);
-  return (
-    <div className="counterContainer">
-      <CounterText count={count} />
-      <button onClick={countAdd}>ボタン +1</button>
-      <button onClick={countSub}>ボタン -1</button>
-      <button onClick={countReset}>リセット</button>
-    </div>
-  );
-};
-
 export default function App() {
-  return <Counter />;
+  return (
+    <>
+      <CounterA />
+      <CounterB />
+      <CounterC />
+    </>
+  );
 }
